b459f47f947e35d45e1208b2f3f73a1b49c76062
Timur Rafikov <rafegg21@gmail.com>
2021-06-10 22:13:15 +0500

    add converter layout

---
+++ src/components/CardConverter.vue
@@ -0,0 +1,49 @@
+<template>
+  <mdb-card class="card">
+    <mdb-card-body>
+      <mdb-card-title class="small">Австарлийский доллар</mdb-card-title>
+      <div class="content">
+        <select class="browser-default custom-select custom-select-sm w-50">
+          <option
+            v-for="(item, index) in selectData"
+            :key="index"
+            :value="item.value"
+          >
+            {{ item.label }}
+          </option>
+        </select>
+        <mdb-input class="w-50" :disabled="!main" />
+      </div>
+    </mdb-card-body>
+  </mdb-card>
+</template>
+
+<script>
+import { mdbCard, mdbCardBody, mdbCardTitle, mdbInput } from "mdbvue";
+export default {
+  name: "CardPage",
+  components: {
+    mdbCard,
+    mdbCardBody,
+    mdbCardTitle,
+    mdbInput,
+  },
+
+  props: {
+    selectData: Array,
+    main: Boolean,
+  },
+};
+</script>
+
+<style scoped>
+.card {
+  max-width: 300px;
+}
+
+.content {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+</style>
--- src/views/CurrencyConverter.vue
+++ src/views/CurrencyConverter.vue
@@ -1,9 +1,39 @@
 <template>
-  <div class="d-flex justify-content-center text-primary">
-    <div class="spinner-border" role="status">
-      <span class="sr-only">Loading...</span>
-    </div>
+  <div class="container">
+    <CardConverter :selectData="selectData" :main="true" />
+    <mdb-icon icon="arrows-alt-h" size="2x" />
+    <CardConverter :selectData="selectData" />
   </div>
 </template>
 
-<script></script>
+<script>
+import { mdbIcon } from "mdbvue";
+import CardConverter from "@/components/CardConverter";
+
+export default {
+  components: {
+    mdbIcon,
+    CardConverter,
+  },
+
+  computed: {
+    selectData() {
+      let data = [];
+      this.$store.state.data.forEach((element) => {
+        data.push({ value: element.Value, label: element.CharCode });
+      });
+      return data;
+    },
+  },
+};
+</script>
+
+<style scoped>
+.container {
+  max-width: 700px;
+  margin: 0 auto;
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+}
+</style>
